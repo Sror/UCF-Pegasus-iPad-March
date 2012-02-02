09d98895b3973feb3356ca5a25029e96e7fba428
Joe Coleman <phantom42@gmail.com>
2012-02-02 13:05:09 -0500

    added example gallery page. not used in actual magazine, but exists as a working code example

---
+++ book/js/dossiers/graduation.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	var myPhotoSwipe = $("#gallery a").photoSwipe({ enableMouseWheel: false, enableKeyboard: false,autoStartSlideshow: true, allowUserZoom: false }) ;
+});
